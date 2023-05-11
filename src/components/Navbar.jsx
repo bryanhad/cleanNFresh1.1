4b4a658e2ce0aa8de08ccb6fb2b2290ba73b703c
bryanhad <124436061+bryanhad@users.noreply.github.com>
2023-05-11 21:54:14 +0700

    tinkering with navbar component
    eheh

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,35 @@
-import { NavLink } from 'react-router-dom'
-import LOGO from '../assets/cleanNFreshLogo.png'
+import { NavLink } from "react-router-dom";
+import LOGO from "../assets/cleanNFreshLogo.png";
+import Burger from "@mui/icons-material/Menu";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
   return (
     <nav className="w-full h-[80px] px-4">
-        <main className="max-w-[1240px] mx-auto h-full flex items-center justify-between">
-            <img className='max-h-[50px]' src={LOGO} alt="" />
+      <main className="max-w-[1240px] mx-auto h-full flex items-center justify-between">
+        <img className="max-h-[50px]" src={LOGO} alt="" />
 
-            <div className='flex gap-4'>
-                <NavLink to='/'>Home</NavLink>
-                <NavLink to='/about'>About</NavLink>
-                <NavLink to='/services'>Services</NavLink>
-                <NavLink to='/faq'>FAQ</NavLink>
-                <NavLink to='/services'>Contact</NavLink>
-            </div>
-        </main>
+        <div className="bg-red-500 h-[50px] w-[50px] relative flex justify-center items-center">
+          <Burger className="fixed z-[2]" onClick={() => setNav(!nav)} />
+        </div>
 
+        {/* burger menu */}
+        <div className={`${nav ? 'flex' : 'hidden' } p-4 bg-slate-300/50 fixed top-0 right-0  w-[60%] h-screen`}>
+          
+        </div>
+
+        <div className="hidden md:flex gap-4  ">
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/about">About</NavLink>
+          <NavLink to="/services">Services</NavLink>
+          <NavLink to="/faq">FAQ</NavLink>
+          <NavLink to="/services">Contact</NavLink>
+        </div>
+      </main>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
