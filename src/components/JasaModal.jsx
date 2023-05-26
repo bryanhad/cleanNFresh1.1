9c56bd458f397d46651fe563ef475aa32b675911
Bryan Hadinata <bryanhadinata76@gmail.com>
2023-05-26 17:58:25 +0700

    Minor change in closing button jasa modal

--- src/components/JasaModal.jsx
+++ src/components/JasaModal.jsx
@@ -7,7 +7,7 @@ const JasaModal = () => {
 
   return (
     <div className='relative '>
-        <div className='h-[12vw] sm:h-[80px] lg:h-[120px] w-full duration-200 relative z-[1] flex items-center py-4 px-6 sm:px-8 border-b-2 border-slate-100'>
+        <div className='h-[12vw] sm:h-[80px] lg:h-[120px] w-full duration-200 relative z-[1] flex items-center py-8 px-6 sm:px-8 border-b-2 border-slate-100'>
             <h1 className='text-primary-darkest relative z-[3] font-[500] text-[4.2vw] sm:text-xl lg:text-[30px]'>{jasa.content.title}</h1>
             {/* <div className='w-full h-full bg-primary-darkest opacity-[20%] absolute top-0 left-0 z-[2]'></div> */}
             {/* <img className='h-full w-full object-cover brightness-[50%] absolute top-0 left-0 z-[1]' src={jasa.content.image} alt="" /> */}
